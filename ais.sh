76c266b2099246dd1c23b73cdbaee294ab05d530
Neil-seu <Neil-seu@users.noreply.github.com>
2017-06-27 03:58:00 +0600

    Update ais.sh

--- ais.sh
+++ ais.sh
@@ -12,8 +12,24 @@ printf "\n"
 read -p "press any key to continue"
 printf '\e[1;33m%-6s\e[m' "### Now use the cfdisk for bios-mbr scheme. This script doesn't support uefi-gpt. So use with caution! ###"
 printf "\n"
-cfdisk
+cfdisk $device
+diskpartcfdisk(){
+  items=$(lsblk -d -p -n -l -o NAME -e 7,11)
+  options=()
+  for item in $items; do
+    options+=("$item" "")
+  done
+  device=$(whiptail --backtitle "$apptitle" --title "$txteditparts (cfdisk)" --menu "" 0 0 0 \
+    "${options[@]}" \
+    3>&1 1>&2 2>&3)
+  if [ "$?" = "0" ]; then
+    clear
+    cfdisk $device
+  fi
+}
 
-printf '\e[1;34m%-6s\e[m' "##################################################################"
+
+
+printf '\e[1;37m%-6s\e[m' "##################################################################"
 printf "\n"
 read -p "press any key to continue"
