cc8bee51a6cda22f7968bd58f6a745d3644d3e74
Neil-seu <Neil-seu@users.noreply.github.com>
2017-06-27 23:01:23 +0600

    Update ais.sh

--- ais.sh
+++ ais.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #ArchInstallerSrcipt
+clear
 printf '\e[1;33m%-6s\e[m' "################## Welcome to the Arch Installer Script #####################"
 printf "\n"
 printf '\e[1;33m%-6s\e[m' "### To increase the root space, this script will automatically trigger the execution ###"
@@ -7,17 +8,17 @@ printf "\n"
 read -p "press any key to continue"
 mount -o remount,size=2G /run/archiso/cowspace
 printf "\n"
+clear
 printf '\e[1;33m%-6s\e[m' "### Success! ###"
 printf "\n"
 read -p "press any key to continue"
+clear
 printf '\e[1;33m%-6s\e[m' "### Now opening the cfdisk for bios-mbr scheme. This script doesn't support uefi-gpt. So use with caution! ###"
 printf "\n"
 #cfdisk $device
 echo "list of DISKs attached (HDD or SSD)"
 lsblk -d | grep disk | awk '{print "/dev/" $1}'
-
 echo ""
-
 echo "Which one to do partition (in full form)"
 read DEVICE_NUMBER
 cfdisk $DEVICE_NUMBER
