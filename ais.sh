b5350887ce821aee95f2ed12a6016022b47ccee3
Neil-seu <Neil-seu@users.noreply.github.com>
2017-06-28 08:23:23 +0600

    Update ais.sh

--- ais.sh
+++ ais.sh
@@ -6,11 +6,22 @@ printf "\n"
 printf '\e[1;33m%-6s\e[m' "### To increase the root space, this script will automatically trigger the execution ###"
 printf "\n"
 read -p "press any key to continue"
+
+## Increasing the cowspace
+
 mount -o remount,size=2G /run/archiso/cowspace
 printf '\e[1;33m%-6s\e[m' "### Success! ###"
 printf "\n"
 read -p "press any key to continue"
 clear
+
+## Unmounting devices in case if any devices are already mounted
+umount /mnt/boot
+umount -R /mnt
+
+
+## Disk Partition
+
 printf '\e[1;33m%-6s\e[m' "### Now opening the cfdisk for bios-mbr scheme. This script doesn't support uefi-gpt. So use with caution! ###"
 printf "\n"
 #cfdisk $device
@@ -20,7 +31,7 @@ printf '\e[1;33m%-6s\e[m' "List of your internal or external devices : "
 lsblk -o name,mountpoint,label,size,uuid
 printf "\n"
 printf '\e[1;33m%-6s\e[m' "Which one to do partition (in full form like /dev/sdX. X means sda/sdb/sdc etc.)"
-echo "Enter your choice:$DEVICE_NUMBER"
+echo "Enter your choice:"
 read DEVICE_NUMBER
 cfdisk $DEVICE_NUMBER
 clear
@@ -28,7 +39,7 @@ printf '\e[1;33m%-6s\e[m' "To format and mount, choose your partition in full fo
 #lsblk -l | grep disk | awk '{print "/dev/" $1}'
 lsblk -o name,mountpoint,label,size,uuid
 printf "\n"
-echo "Enter your choice:$DEVICE_NUMBER"
+echo "Enter your choice:"
 read DEVICE_NUMBER
 printf '\e[1;33m%-6s\e[m' "formatting..."
 mkfs.ext4 $DEVICE_NUMBER
