bf57c8d0fa9e314823125e458c95204a67d87353
Neil-seu <Neil-seu@users.noreply.github.com>
2017-06-28 08:02:16 +0600

    Update ais.sh

--- ais.sh
+++ ais.sh
@@ -14,28 +14,30 @@ clear
 printf '\e[1;33m%-6s\e[m' "### Now opening the cfdisk for bios-mbr scheme. This script doesn't support uefi-gpt. So use with caution! ###"
 printf "\n"
 #cfdisk $device
-echo "list of DISKs attached (HDD or SSD)"
+#echo "list of DISKs attached (HDD or SSD)"
+printf '\e[1;33m%-6s\e[m' "List of your internal or external devices : "
 #lsblk -l | grep disk | awk '{print "/dev/" $1}'
 lsblk -o name,mountpoint,label,size,uuid
 echo ""
-echo "Which one to do partition (in full form like /dev/sdX. X means sda/sdb/sdc etc.)"
-echo "Enter your choice:$DEVICE_NUMBER"
+printf '\e[1;33m%-6s\e[m' "Which one to do partition (in full form like /dev/sdX. X means sda/sdb/sdc etc.)"
+printf "\n"
+printf '\e[1;33m%-6s\e[m' "Enter your choice:$DEVICE_NUMBER"
 read DEVICE_NUMBER
 cfdisk $DEVICE_NUMBER
 clear
-echo "To format and mount, choose your partition in full form you have just created from the list below :"
+printf '\e[1;33m%-6s\e[m' "To format and mount, choose your partition in full form you have just created from the list below :"
 #lsblk -l | grep disk | awk '{print "/dev/" $1}'
 lsblk -o name,mountpoint,label,size,uuid
-echo ""
+printf "\n"
+printf '\e[1;33m%-6s\e[m' "Enter your choice:$DEVICE_NUMBER"
 read DEVICE_NUMBER
-echo "formatting..."
+printf '\e[1;33m%-6s\e[m' "formatting..."
 mkfs.ext4 $DEVICE_NUMBER
-echo "format successful"
-echo "mounting root partition..."
+printf '\e[1;33m%-6s\e[m' "format successful"
+printf '\e[1;33m%-6s\e[m' "mounting root partition..."
 mount $DEVICE_NUMBER /mnt
-echo "mount successful"
+printf '\e[1;33m%-6s\e[m' "mount successful"
 printf '\e[1;33m%-6s\e[m' "### Success! ###"
-clear
-printf '\e[1;37m%-6s\e[m' "##################################################################"
 printf "\n"
 read -p "press any key to continue"
+clear
