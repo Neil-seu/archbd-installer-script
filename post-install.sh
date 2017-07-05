76370873671095e4d07a5d8d54965efc1ba7046a
Neil-seu <Neil-seu@users.noreply.github.com>
2017-07-05 22:19:45 +0600

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -56,3 +56,47 @@ echo "#####################################################################"
 printf "\n"
 printf "\n"
 clear
+
+##### Installing Desktop environment and necessary drivers
+printf '\e[1;33m%-6s\e[m' "######### Now Installing a Desktop environment: #########"
+printf "\n"
+sed -i -e '$a\\n[archlinuxfr]\nServer = http://repo.archlinux.fr/$arch\nSigLevel = Never' /mnt/etc/pacman.conf
+pacman -Syy yaourt xf86-video-vesa mesa xorg-server xorg-utils xorg-xinit xterm xfce4 unrar unzip p7zip lzop cpio xarchiver xfce4-goodies gtk-engine-murrine lightdm-gtk-greeter --noconfirm
+printf "\n"
+echo "Enabling login manager services..."
+systemctl enable lightdm.service
+echo "Done!"
+printf "\n"
+echo "Now choose your gpu to install it's driver :"
+OPTIONS="nvidia amd intel"
+      select opt in $OPTIONS; do
+            if [ "$opt" = "nvidia" ]; then
+                pacman -Syy lib32-mesa-libgl xf86-video-nouveau --noconfirm
+                exit
+               elif [ "$opt" = "amd" ]; then
+                pacman -Syy xf86-video-amdgpu xf86-video-ati lib32-mesa-libgl --noconfirm
+                exit
+               elif [ "$opt" = "intel" ]; then
+                pacman -Syy xf86-video-intel lib32-mesa-libgl --noconfirm
+                exit
+             fi
+       done
+echo "All drivers are successfully installed!"
+printf "\n"
+read -p "press enter to continue..."
+clear
+
+
+#### Installing Some common softwares
+printf '\e[1;33m%-6s\e[m' "######### Would you mind to install some common software? I guess not! Let's do this: #########"
+printf "\n"
+pacman -Syyu chromium firefox deluge codeblocks gimp gpick vlc smplayer smplayer-skins simplescreenrecorder gparted htop libreoffice-fresh bleachbit thunderbird --noconfirm
+printf "\n"
+echo "Success!"
+read -p "press enter to continue..."
+clear
+
+printf '\e[1;33m%-6s\e[m' "######### All work is done!! #########"
+printf "\n"
+read -p "press enter key to reboot..."
+reboot
