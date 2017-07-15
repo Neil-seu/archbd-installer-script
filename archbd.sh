92a28e7d0b7bfb96096783a9190fefb1182ea910
Neil-seu <Neil-seu@users.noreply.github.com>
2017-07-15 21:36:13 +0600

    Update archbd.sh

--- archbd.sh
+++ archbd.sh
@@ -160,7 +160,7 @@ echo "Enter the username:"
 printf "\n"
 read usr
 arch-chroot /mnt useradd -m -g users -G storage,power,wheel -s /bin/bash $usr
-sed -i 's/^#\%wheel ALL=(ALL) ALL/\%wheel ALL=(ALL) ALL/' /mnt/etc/sudoers
+arch-chroot /mnt sed -i 's/^#\%wheel ALL=(ALL) ALL/\%wheel ALL=(ALL) ALL/' /etc/sudoers
 printf "\n"
 echo "Enter the password for the user:"
 printf "\n"
