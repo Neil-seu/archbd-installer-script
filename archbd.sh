14843716fa2eb842f30d4d8131bb16caedfb0a51
Neil-seu <Neil-seu@users.noreply.github.com>
2017-07-16 02:43:28 +0600

    Update archbd.sh

--- archbd.sh
+++ archbd.sh
@@ -274,11 +274,11 @@ printf "\n"
 printf "Enter the number:"
 read gpu
 	if [ "$gpu" = 1 ]; then
-		arch-chroot /mnt pacman -Syyu lib32-mesa-libgl xf86-video-nouveau --noconfirm
+		arch-chroot /mnt pacman -Syyu lib32-mesa-libgl xf86-video-nouveau opengl --noconfirm
 	elif [ "$gpu" = 2 ]; then
-		arch-chroot /mnt pacman -Syyu xf86-video-amdgpu xf86-video-ati lib32-mesa-libgl --noconfirm
+		arch-chroot /mnt pacman -Syyu xf86-video-amdgpu xf86-video-ati lib32-mesa-libgl opengl --noconfirm
 	elif [ "$gpu" = 3 ]; then
-		arch-chroot /mnt pacman -Syyu xf86-video-intel lib32-mesa-libgl --noconfirm
+		arch-chroot /mnt pacman -Syyu xf86-video-intel lib32-mesa-libgl opengl --noconfirm
 	else 
 		echo "Unknown parameter"
 	fi
