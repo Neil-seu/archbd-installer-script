145c6f282f6f707709ea0418b5dfb075af6492d1
Neil-seu <Neil-seu@users.noreply.github.com>
2017-07-19 13:40:18 +0600

    Update archbd.sh

--- archbd.sh
+++ archbd.sh
@@ -179,22 +179,30 @@ read -p "press enter to continue..."
 printf "\n"
 echo "#####################################################################"
 printf "\n"
-printf '\e[1;33m%-6s\e[m' "## Now detecting and enabling your network devices: ##"
-wireless_dev=`ip link | grep wl | awk '{print $2}' | sed 's/://'`
+clear
+printf "Which network interface do you use: \n1. Wireless\n2. Wired\n"
 printf "\n"
-echo " $wireless_dev is found as your wireless device. Enabling... "
-arch-chroot /mnt systemctl enable dhcpcd@${wireless_dev}.service
-printf "\n"
-echo " SUCCESS! "
-printf "\n"
-read -p "press enter to continue..."
-printf "\n"
-wired_dev=`ip link | grep "ens\|eno\|enp" | awk '{print $2}' | sed 's/://'`
-echo " $wired_dev is found as your lan device. Enabling... "
-arch-chroot /mnt systemctl enable dhcpcd@${wired_dev}.service
-echo " SUCCESS! "
-printf "\n"
-read -p "press enter to continue..."
+printf "Enter the number:"
+read interface
+	if [ "$interface" = 1 ]; then
+		printf '\e[1;33m%-6s\e[m' "## Now detecting and enabling your network devices: ##"
+		wireless_dev=`ip link | grep wl | awk '{print $2}' | sed 's/://'`
+		printf "\n"
+		echo " $wireless_dev is found as your wireless device. Enabling... "
+		arch-chroot /mnt systemctl enable dhcpcd@${wireless_dev}.service
+		printf "\n"
+		echo " SUCCESS! "
+		printf "\n"
+		read -p "press enter to continue..."
+	elif [ "$interface" = 2 ]; then	
+		##printf "\n"
+		wired_dev=`ip link | grep "ens\|eno\|enp" | awk '{print $2}' | sed 's/://'`
+		echo " $wired_dev is found as your lan device. Enabling... "
+		arch-chroot /mnt systemctl enable dhcpcd@${wired_dev}.service
+		echo " SUCCESS! "
+		printf "\n"
+		read -p "press enter to continue..."
+	fi	
 printf "\n"
 echo "Enabling Network manager service during boot..."
 arch-chroot /mnt systemctl enable NetworkManager.service
@@ -205,8 +213,8 @@ read -p "press enter to continue..."
 printf "\n"
 echo "Enabling other necessary services..."
 arch-chroot /mnt systemctl enable bluetooth.service
-arch-chroot /mnt systemctl enable ppp@${wired_dev}.service
-arch-chroot /mnt systemctl enable ntpd.service
+##arch-chroot /mnt systemctl enable ppp@${wired_dev}.service
+##arch-chroot /mnt systemctl enable ntpd.service
 echo "DONE!"
 printf "\n"
 read -p "press enter to continue..."
