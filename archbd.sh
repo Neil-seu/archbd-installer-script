c5b22f3deb194f8cf97ee6b34cfaaa2b597bd276
Neil-seu <Neil-seu@users.noreply.github.com>
2017-07-22 16:38:51 +0600

    Update archbd.sh

--- archbd.sh
+++ archbd.sh
@@ -31,7 +31,7 @@ printf "\n"
     	  pacman-key --refresh-keys
 printf '\e[1;32m%-6s\e[m' "Updated pacman keys successfully!" 
 printf "\n"		
-pacman -Syy archlinux-keyring --noconfirm
+pacman -Syy archlinux-keyring reflector --noconfirm
 printf "\n"
 printf '\e[1;32m%-6s\e[m' "### Success! ###"
 printf "\n"
@@ -81,12 +81,16 @@ clear
 
 
 #### Installing the base system 
-printf '\e[1;33m%-6s\e[m' "Now Ranking mirrorlist, Please wait... "
+echo "Now choose any mirror :"
 printf "\n"
-	cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-	sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
-	rankmirrors -n 10 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
-	rm /etc/pacman.d/mirrorlist.backup
+printf "\n"
+	echo "("Australia--AU" || "Austria--AT" || "Belarus--BY" || "Belgium--BE" || "Brazil--BR" || "Bulgaria--BG" || "Canada--CA" || "Chile--CL" || "China--CN" || "Colombia--CO" || "Czech Republic--CZ" || "Denmark--DK" || "Estonia--EE" || "Finland--FI" || "France--FR" || "Germany--DE" || "Greece--GR" || "Hong Kong--HK" || "Hungary--HU" || "Indonesia--ID" || "India--IN" || "Ireland--IE" || "Israel--IL" || "Italy--IT" || "Japan--JP" || "Kazakhstan--KZ" || "Korea--KR" || "Latvia--LV" || "Luxembourg--LU" || "Macedonia--MK" || "Netherlands--NL" || "New Caledonia--NC" || "New Zealand--NZ" || "Norway--NO" || "Poland--PL" || "Portugal--PT" || "Romania--RO" || "Russian--RU" || "Serbia--RS" || "Singapore--SG" || "Slovakia--SK" || "South Africa--ZA" || "Spain--ES" || "Sri Lanka--LK" || "Sweden--SE" || "Switzerland--CH" || "Taiwan--TW" || "Turkey--TR" || "Ukraine--UA" || "United Kingdom--GB" || "United States--US" || "Uzbekistan--UZ" || "VietNam--VN")"
+	printf "\n"
+	echo "Enter your country name:"
+	read COUNTRY_NAME
+	printf '\e[1;33m%-6s\e[m' "##  Configuring and ranking arch mirror list. Please wait... ##"
+	reflector --verbose --country '$COUNTRY_NAME' -p http --sort rate --save /etc/pacman.d/mirrorlist
+
 printf "\n"
 printf "\n"
 printf '\e[1;32m%-6s\e[m' "##  Mirrorlist Successfully Generated! ##" 
