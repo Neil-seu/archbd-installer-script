7d2b3448152125f0b9bb1abeb9adffec5ea78c30
Neil-seu <Neil-seu@users.noreply.github.com>
2017-08-01 00:48:55 +0600

    Update mirror-test.sh

--- mirror-test.sh
+++ mirror-test.sh
@@ -1,18 +1,7 @@
 echo "Now choose any mirror :"
 printf "\n"
 printf "\n"
-countries=("
-1. ALL			11. Colombia--CO		21. Indonesia--ID		31. Macedonia--MK  		41. Singapore--SG   	51. United Kingdom--GB
-2. Australia--AU	12. Czech Republic--CZ		22. India--IN			32. Netherlands--NL		42. Slovakia--SK	52. United States--US
-3. Austria--AT 		13. Denmark--DK			23. Ireland--IE			33. New Caledonia--NC		43. South Africa--ZA	53. Uzbekistan--UZ
-4. Belarus--BY		14. Estonia--EE			24. Israel--IL			34. New Zealand--NZ		44. Spain--ES		54. VietNam--VN
-5. Belgium--BE		15. Finland--FI			25. Italy--IT			35. Norway--NO			45. Sri Lanka--LK
-6. Brazil--BR		16. France--FR			26. Japan--JP			36. Poland--PL			46. Sweden--SE
-7. Bulgaria--BG		17. Germany--DE			27. Kazakhstan--KZ		37. Portugal--PT		47. Switzerland--CH
-8. Canada--CA		18. Greece--GR			28. Korea--KR			38. Romania--RO			48. Taiwan--TW
-9. Chile--CL		19. Hong Kong--HK		29. Latvia--LV			39. Russian--RU			49. Turkey--TR
-10. China--CN		20. Hungary--HU			30. Luxembourg--LU		40. Serbia--RS			50. Ukraine--UA
-")
+countries=(" 1. Australia--AU 2. Austria--AT 3. Belarus--BY 4. Belgium--BE 5. Brazil--BR 6. Bulgaria--BG 7. Canada--CA 8. Chile--CL 9. China--CN 10. Colombia--CO 11. Czech Republic--CZ 12. Denmark--DK 13. Estonia--EE 14. Finland--FI 15. France--FR 16. Germany--DE 17. Greece--GR 18. Hong Kong--HK 19. Hungary--HU 20. Indonesia--ID 21. India--IN 22. Ireland--IE 23. Israel--IL 24. Italy--IT 25. Japan--JP 26. Kazakhstan--KZ 27. Korea--KR 28. Latvia--LV 29. Luxembourg--LU 30. Macedonia--MK 31. Netherlands--NL 32. New Caledonia--NC 33. New Zealand--NZ 34. Norway--NO 35. Poland--PL 36. Portugal--PT 37. Romania--RO 38. Russian--RU 39. Serbia--RS 40. Singapore--SG 41. Slovakia--SK 42. South Africa--ZA 43. Spain--ES 44. Sri Lanka--LK 45. Sweden--SE 46. Switzerland--CH 47. Taiwan--TW 48. Turkey--TR 49. Ukraine--UA 50. United Kingdom--GB 51. United States--US 52. Uzbekistan--UZ 53. VietNam--VN " )
 for names in "${countries[@]}"; do
 	printf "%-8s\n" "${names}"
 done | column
