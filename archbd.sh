42551d9492a326adfe83bb256057f57095cd45ab
Neil-seu <Neil-seu@users.noreply.github.com>
2017-08-01 22:32:35 +0600

    Update archbd.sh

--- archbd.sh
+++ archbd.sh
@@ -94,35 +94,35 @@ printf "\n"
 read choice
 printf "\n"
 	if [[ "$choice" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
-	echo "Now choose any mirror :"
-	printf "\n"
-	printf "\n"
-	countries=(" 1. Australia--AU 2. Austria--AT 3. Belarus--BY 4. Belgium--BE 5. Brazil--BR 6. Bulgaria--BG 7. Canada--CA 8. Chile--CL 9. China--CN 10. Colombia--CO 11. Czech Republic--CZ 12. Denmark--DK 13. Estonia--EE 14. Finland--FI 15. France--FR 16. Germany--DE 17. Greece--GR 18. Hong Kong--HK 19. Hungary--HU 20. Indonesia--ID 21. India--IN 22. Ireland--IE 23. Israel--IL 24. Italy--IT 25. Japan--JP 26. Kazakhstan--KZ 27. Korea--KR 28. Latvia--LV 29. Luxembourg--LU 30. Macedonia--MK 31. Netherlands--NL 32. New Caledonia--NC 33. New Zealand--NZ 34. Norway--NO 35. Poland--PL 36. Portugal--PT 37. Romania--RO 38. Russian--RU 39. Serbia--RS 40. Singapore--SG 41. Slovakia--SK 42. South Africa--ZA 43. Spain--ES 44. Sri Lanka--LK 45. Sweden--SE 46. Switzerland--CH 47. Taiwan--TW 48. Turkey--TR 49. Ukraine--UA 50. United Kingdom--GB 51. United States--US 52. Uzbekistan--UZ 53. VietNam--VN " )
-	printf "\n"
-	echo -e "$countries" | column -t -s ':'
-	printf "\n"
-	printf "\n"
-	echo "Enter your country code:"
-	printf "\n"
-	read COUNTRY_CODE
-	printf '\e[1;33m%-6s\e[m' "##  Configuring and ranking arch mirror list. Please wait... ##"
-	url="https://www.archlinux.org/mirrorlist/?country=${COUNTRY_CODE}&use_mirror_status=on"
-	## making a temporary file where chosen mirror list will be placed and place it in a variable
-	tempfile=$(mktemp --suffix=-mirrorlist)
-	## Getting latest mirrorlist and saving to temp file
-	wget -qO- "$url" | sed 's/^#Server/Server/g' > "$tempfile"
-	## Backing up and place the new mirrorlist
-	mv -i /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-	mv -i "$tempfile" /etc/pacman.d/mirrorlist
-	cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.tmp
-	rankmirrors /etc/pacman.d/mirrorlist.tmp > /etc/pacman.d/mirrorlist
-	rm /etc/pacman.d/mirrorlist.tmp
-	printf "\n"
-	printf "\n"
-	printf '\e[1;32m%-6s\e[m' "##  Mirrorlist Successfully Generated! ##" 
-	printf "\n"
-	read -p "press enter to continue..."
-	clear
+		echo "Now choose any mirror :"
+		printf "\n"
+		printf "\n"
+		countries=(" 1. Australia--AU 2. Austria--AT 3. Belarus--BY 4. Belgium--BE 5. Brazil--BR 6. Bulgaria--BG 7. Canada--CA 8. Chile--CL 9. China--CN 10. Colombia--CO 11. Czech Republic--CZ 12. Denmark--DK 13. Estonia--EE 14. Finland--FI 15. France--FR 16. Germany--DE 17. Greece--GR 18. Hong Kong--HK 19. Hungary--HU 20. Indonesia--ID 21. India--IN 22. Ireland--IE 23. Israel--IL 24. Italy--IT 25. Japan--JP 26. Kazakhstan--KZ 27. Korea--KR 28. Latvia--LV 29. Luxembourg--LU 30. Macedonia--MK 31. Netherlands--NL 32. New Caledonia--NC 33. New Zealand--NZ 34. Norway--NO 35. Poland--PL 36. Portugal--PT 37. Romania--RO 38. Russian--RU 39. Serbia--RS 40. Singapore--SG 41. Slovakia--SK 42. South Africa--ZA 43. Spain--ES 44. Sri Lanka--LK 45. Sweden--SE 46. Switzerland--CH 47. Taiwan--TW 48. Turkey--TR 49. Ukraine--UA 50. United Kingdom--GB 51. United States--US 52. Uzbekistan--UZ 53. VietNam--VN " )
+		printf "\n"
+		echo -e "$countries" | column -t -s ':'
+		printf "\n"
+		printf "\n"
+		echo "Enter your country code:"
+		printf "\n"
+		read COUNTRY_CODE
+		printf '\e[1;33m%-6s\e[m' "##  Configuring and ranking arch mirror list. Please wait... ##"
+		url="https://www.archlinux.org/mirrorlist/?country=${COUNTRY_CODE}&use_mirror_status=on"
+		## making a temporary file where chosen mirror list will be placed and place it in a variable
+		tempfile=$(mktemp --suffix=-mirrorlist)
+		## Getting latest mirrorlist and saving to temp file
+		wget -qO- "$url" | sed 's/^#Server/Server/g' > "$tempfile"
+		## Backing up and place the new mirrorlist
+		mv -i /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+		mv -i "$tempfile" /etc/pacman.d/mirrorlist
+		cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.tmp
+		rankmirrors /etc/pacman.d/mirrorlist.tmp > /etc/pacman.d/mirrorlist
+		rm /etc/pacman.d/mirrorlist.tmp
+		printf "\n"
+		printf "\n"
+		printf '\e[1;32m%-6s\e[m' "##  Mirrorlist Successfully Generated! ##" 
+		printf "\n"
+		read -p "press enter to continue..."
+		clear
 	
 	elif [[ "$choice" =~ ^([nN][oO]|[nN])+$ ]]; then
 		break	
@@ -366,7 +366,8 @@ read number
 		arch-chroot /mnt systemctl enable sddm.service
 		##sed -i -e '$a\exec startkde' /mnt/home/$USERNAME/.xinitrc	
 	else
-		echo "Unknown parameter"	
+		echo "Unknown parameter"
+		break
 	fi		
 	
 printf "\n"
@@ -386,6 +387,7 @@ read gpu
 		arch-chroot /mnt pacman -Syu xf86-video-intel lib32-mesa-libgl --noconfirm
 	else 
 		echo "Unknown parameter"
+		break
 	fi
 	
 printf "\n"     
