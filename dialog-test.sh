7ff7f9b257acd21fa73a5c611858000b19a378d6
Neil-seu <Neil-seu@users.noreply.github.com>
2017-08-21 12:13:23 +0000

    Update dialog-test.sh

--- dialog-test.sh
+++ dialog-test.sh
@@ -4,6 +4,21 @@ dialog --backtitle "Archbd Installer Script" --yesno "Welcome to Arch Installer.
 response=$?
 clear
 case $response in
-    0) sudo pacman -Syyu;;
+    0) mount -o remount,size=4G /run/archiso/cowspace;;
+       printf "\n";;
+       printf '\e[1;33m%-6s\e[m' "Updating pacman keys....";;
+       printf "\n";;
+	   pacman-db-upgrade;;
+	   pacman-key --init;;
+	   pacman-key --populate archlinux;;
+       pacman-key --refresh-keys;;
+       dialog --infobox "Updated pacman keys successfully!" 10 30;; 
+       sleep 3;;
+       clear;;
+       pacman -Syy archlinux-keyring --noconfirm;;
+       printf "\n";;
+       dialog --infobox "Successful!" 10 20;;
+       sleep 3;;
+       clear;;
     1) exit;;
 esac
