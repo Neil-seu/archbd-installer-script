5817ebefc94d36c56e2dd950ee032aaaae7a7b0c
Neil-seu <Neil-seu@users.noreply.github.com>
2017-08-24 22:28:25 +0600

    Update dialog-test.sh

--- dialog-test.sh
+++ dialog-test.sh
@@ -25,13 +25,53 @@ if [[ "$response" -eq "Yes" ]]
        break
 fi
 
-dialog --backtitle "Archbd Installer Script" --infobox "Unmounting devices in case if any devices are already mounted. Please wait..." 10 40
-umount -R /mnt
-clear
+dialog --backtitle "Archbd Installer Script" --infobox "Unmounting devices in case if any devices are already mounted. \
+	Please wait..." 10 40
+	umount -R /mnt
+	clear
 
-dialog --backtitle "Archbd Installer Script" --yesno "Now opening the cfdisk for bios-mbr scheme. This script doesn't support uefi-gpt. So use with caution! Proceed?" 10 40
-response=$?
-clear
-if [[ "$response" -eq "Yes" ]]
-    then
-    
+dialog --backtitle "Archbd Installer Script" --yesno "Now opening the cfdisk for bios-mbr scheme. \
+	This script doesn't support uefi-gpt. \
+	So use with caution! \
+	Press No if you don't want to configure your partition. Proceed?" 10 70
+	response=$?
+	clear
+	if [[ "$response" -eq "Yes" ]] then
+    		printf '\e[1;33m%-6s\e[m' "List of your internal or external devices : "
+		printf '\n'
+		printf '\n'
+		lsblk -o name,mountpoint,label,size,uuid
+		printf "\n"
+		printf "\n"
+		printf '\e[1;33m%-6s\e[m' "Which one to do partition? Type it in full form like /dev/sdX.\n X means sda/sdb/sdc etc."
+		printf "\n"
+		printf "\n"
+    		echo "Enter your choice:"
+		read DEVICE_NUMBER
+		cfdisk $DEVICE_NUMBER
+		clear
+		printf '\e[1;33m%-6s\e[m' "To format and mount, choose your partition in full form like /dev/sdX() you have just created from the list below :"
+		printf "\n"
+		printf "\n"
+		lsblk -o name,mountpoint,label,size,uuid
+		printf "\n"
+		echo "Enter your choice:"
+		read DEVICE_NUMBER
+		printf '\e[1;33m%-6s\e[m' "formatting..."
+		mkfs.ext4 $DEVICE_NUMBER
+		printf '\e[1;32m%-6s\e[m' "format successful!"
+		printf "\n"
+		printf '\e[1;33m%-6s\e[m' "mounting root partition..."
+		mount $DEVICE_NUMBER /mnt
+		printf "\n"
+		printf '\e[1;32m%-6s\e[m' "mount successful!"
+		printf "\n"
+		printf '\e[1;32m%-6s\e[m' "### Success! ###"
+		printf "\n"
+		read -p "press enter to continue..."
+		clear
+	else
+		break
+	fi
+	
+	
